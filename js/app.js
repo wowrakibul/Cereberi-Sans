a09567a17bd0587841b9cdd315d74bd32667ffec
Wow Rakibul <74898633+wowrakibul@users.noreply.github.com>
2024-06-13 19:44:07 +0600

    Delete js directory

--- js/app.js
+++
@@ -1,6 +0,0 @@
-var myElement = document.querySelector('body');
-// construct an instance of Headroom, passing the element
-
-var headroom  = new Headroom(myElement);
-// initialise
-headroom.init(); 
